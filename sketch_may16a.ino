218ec3f695fb27f6d93d33f6d0c225778dc23a5e
kozicka238 <julien280108@gmail.com>
2025-05-16 22:33:55 +0300

    Add arduino sketch
    this is the first version of the code for the rotation mechanism of the mini observatory for the project

---
+++ sketch_may16a.ino
@@ -0,0 +1,33 @@
+#include <Stepper.h>
+const int stepsPerRevolution = 4096;
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+int br;
+void setup() {
+  myStepper.setSpeed(5);  // Slow speed for testing
+  Serial.begin(9600);
+  Serial.println("Send 'R' to rotate 90° clockwise.");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == 'R') {
+      br++;
+      switch(br)
+      {
+        case 1: Serial.print("mercury "); break;
+        case 2: Serial.print("Venus "); break;
+        case 3: Serial.print("Mars "); break;
+        case 4: Serial.print("Jupiter "); break;
+        case 5: Serial.print("Satrun "); break;
+        case 6: Serial.print("Uranus "); break;
+        case 7: Serial.print("Neptun "); break;
+        case 8: Serial.print("Pluto "); break;
+        case 9: br = 0; return;
+      }
+      Serial.println("Rotating 90°...");
+      myStepper.step(256);  // 512 steps = 90° (2048 steps/360°)
+      Serial.println("Done!");
+    }
+  }
+}
